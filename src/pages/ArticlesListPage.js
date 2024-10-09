2372656b11154708c35672f012382a8530262446
uilianrocha <uilianrrocha@gmail.com>
2024-10-09 11:10:59 -0300

    Implement Link to ArticlesListPage

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlesListPage = () => {
-    return <h1>This is the articles list page!</h1>;
-  };
-  
-  export default ArticlesListPage;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((article) => (
+        <Link className="article-list-item" to={`/articles/${article.name}`}>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default ArticlesListPage;
