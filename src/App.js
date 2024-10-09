db87c0b69131b143a16e75b69d9f465e6ab842c8
uilianrocha <uilianrrocha@gmail.com>
2024-10-09 17:31:00 -0300

    Update NotFoundPage component name

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles" element={<ArticlesListPage />} />
             <Route path="/articles/:articleId" element={<ArticlePage />} />
-            <Route path="*" element={<NotFoundPagePage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </div>
