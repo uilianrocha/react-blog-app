4934367c54245a99d1eebd8657004e138163423e
uilianrocha <uilianrrocha@gmail.com>
2024-10-09 11:16:45 -0300

    Add key prop to Link components

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -6,7 +6,11 @@ const ArticlesListPage = () => {
     <>
       <h1>Articles</h1>
       {articles.map((article) => (
-        <Link className="article-list-item" to={`/articles/${article.name}`}>
+        <Link
+          key={article.name}
+          className="article-list-item"
+          to={`/articles/${article.name}`}
+        >
           <h3>{article.title}</h3>
           <p>{article.content[0].substring(0, 150)}...</p>
         </Link>
