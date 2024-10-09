b6127d5c867391bfee350797130fa2cd4d1b6584
uilianrocha <uilianrrocha@gmail.com>
2024-10-09 18:39:45 -0300

    Add if condition for NotFoundPage based on route

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,16 @@
 import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
 
 const ArticlePage = () => {
   const params = useParams();
   const articleId = params.articleId;
   const article = articles.find((article) => article.name === articleId);
+
+  if (!article) {
+    return <NotFoundPage />;
+  }
+
   return (
     <>
       <h1>{article.title}</h1>
