593f0ecfb145f0471b982bfa0c32ba8ee8da0353
uilianrocha <uilianrrocha@gmail.com>
2024-10-08 21:34:10 -0300

    First App.js commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+      <div className="App">
+        <h1>My awesome blog</h1>
+        <div id="page-body">
+            Welcome to my blog
+        </div>
+      </div>
+  );
+}
+
+export default App;
