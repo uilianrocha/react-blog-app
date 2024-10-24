ce5532cf7885fbd46afe49f570770f5574158645
uilianrocha <uilianrrocha@gmail.com>
2024-10-24 19:21:34 -0300

    LoginPage initial commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import "./css/LoginPage.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
