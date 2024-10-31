37871c655bb424b748d77d84dc981a94ab2b2d8c
uilianrocha <uilianrrocha@gmail.com>
2024-10-31 20:15:26 -0300

    Create LoginPage

--- react-blog-frontend/src/NavBar.js
+++ react-blog-frontend/src/NavBar.js
@@ -13,6 +13,9 @@ const NavBar = () => {
         <li>
           <Link to="/articles">Articles</Link>
         </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
   );
