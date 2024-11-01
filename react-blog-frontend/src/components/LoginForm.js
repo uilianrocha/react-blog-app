177442e0e52fd992da86b2ccd3857dd7fad24c5f
uilianrocha <uilianrrocha@gmail.com>
2024-11-01 15:58:41 -0300

    Create Link to register new account

--- react-blog-frontend/src/components/LoginForm.js
+++ react-blog-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
@@ -31,6 +32,12 @@ const LoginForm = () => {
       <button type="submit" className="login-button">
         Login
       </button>
+      <p className="signup-text">
+        New to our blog?{" "}
+        <Link to="/signup" className="signup-link">
+          Create an account!
+        </Link>
+      </p>
     </form>
   );
 };
