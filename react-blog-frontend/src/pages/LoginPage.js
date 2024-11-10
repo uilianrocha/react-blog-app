6fb7221b2f879954fe924b6180d821a02aa92f21
uilianrocha <uilianrrocha@gmail.com>
2024-11-10 12:33:50 -0300

    Update LoginPage

--- react-blog-frontend/src/pages/LoginPage.js
+++ react-blog-frontend/src/pages/LoginPage.js
@@ -1,14 +1,36 @@
 import "./css/LoginPage.css";
+import React, { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import SignupForm from "../components/SignupForm";
 
 const LoginPage = () => {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const toggleForm = () => {
+    setIsSignup((prev) => !prev);
+  };
+
   return (
     <div className="login-container">
-      <h1>Welcome to our Blog</h1>
-      <p className="login-text">Please sign in to your account:</p>
-      <LoginForm />
+      <h1>{isSignup ? "Create an Account" : "Welcome to our Blog"}</h1>
+      <p className="login-text">
+        {isSignup
+          ? "Sign up for a new account:"
+          : "Please sign in to your account:"}
+      </p>
+
+      {isSignup ? <SignupForm /> : <LoginForm />}
+
+      <p className="toggle-text">
+        {isSignup ? "Already have an account?" : "New to our blog?"}{" "}
+        <button onClick={toggleForm} className="toggle-link">
+          {isSignup ? "Login here" : "Create an account"}
+        </button>
+      </p>
     </div>
   );
 };
 
 export default LoginPage;
+
+
